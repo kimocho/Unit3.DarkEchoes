a2464b726fb04e7058b09059255843c3f78239a4
Samuel Kim <samjkim455@gmail.com>
2025-06-05 19:31:53 -0400

    first draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { episodeList } from './data.js';
+import { useState } from 'react';
+
 export default function App() {
   // TODO
+  const [episodeList, setEpisodeList] = useState(episodeList);
+  return (
+    <>
+      <ol>
+        {episodeList.map(elem => {
+          return (`<li>${elem.title}</li>`);
+        }
+        )}
+      </ol>
+    </>
+  );
 }

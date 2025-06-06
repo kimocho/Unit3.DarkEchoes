d6be84c0635d24dc67154f903321b62c1dc93a6c
Samuel Kim <samjkim455@gmail.com>
2025-06-05 23:35:24 -0400

    i may have finally completed it roughly

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import { episodeList } from './data.js';
 
 const App = () => {
   const [episodeObj, setEpisodeObj] = useState(episodeList[0]);
-  // onClick={setEpisodeObj({ ...obj })}
   return (
     <>
       <h2>Dark Echoes</h2>
       <p>Episodes</p>
       <section>
         <ul>
-          {episodeList.map((obj) => <li key={obj.id}> {obj.title}</li>)}
+          {episodeList.map((obj) => <li key={obj.id} onClick={() => setEpisodeObj({ ...obj })}> {obj.title}</li>)}
         </ul >
-        <ul>
+        <ol>
+          <h2>Select an Episode</h2>
           <li>Episode {episodeObj.id}</li>
           <li>{episodeObj.title}</li>
           <li>{episodeObj.description}</li>
           <button>Watch Now</button>
-        </ul>
+        </ol>
       </section>
     </>
   )
